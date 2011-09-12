726a6b9b4512ccfd8a9570a50934019d62a05d8a
a109639 <johan.poirier@atosorigin.com>
2011-09-12 15:40:29 +0200

    added bitbucket link in footer public & private

--- include/theme/cdm2011/footer_private.php
+++ include/theme/cdm2011/footer_private.php
@@ -1,4 +1,4 @@
 <div id="footer">
-    <p><a href="index.php">Accueil</a> | <a href="/?op=account">Mon compte</a> | <a href="/?op=rules">Règlement</a> | <a href="mailto:johan.poirier@gmail.com">E-mail</a> | <a href="/?op=logout">Déconnexion</a></p>
+    <p><a href="index.php">Accueil</a> | <a href="/?op=account">Mon compte</a> | <a href="/?op=rules">Règlement</a> | <a href="https://bitbucket.org/dst17/cdm2011/issues" target="_blank">Soummettre un bug sur Bitbucket</a> | <a href="mailto:johan.poirier@gmail.com">E-mail</a> | <a href="/?op=logout">Déconnexion</a></p>
     <p>©2011 JoPs / Lampin.net</p>
 </div>
--- include/theme/cdm2011/footer_public.php
+++ include/theme/cdm2011/footer_public.php
@@ -1,4 +1,4 @@
 <div id="footer">
-    <p><a href="/?op=register">S'inscrire</a> | <a href="/?op=forgot_ids">J'ai oublié mes identifiants de connexion</a> | <a href="https://bitbucket.org/dst17/cdm2011/issues" target="_blank">Projet sur Bitbucket</a> | <a href="mailto:johan.poirier@gmail.com">E-mail</a></p>
+    <p><a href="/?op=register">S'inscrire</a> | <a href="/?op=forgot_ids">J'ai oublié mes identifiants de connexion</a> | <a href="https://bitbucket.org/dst17/cdm2011" target="_blank">Projet sur Bitbucket</a> | <a href="mailto:johan.poirier@gmail.com">E-mail</a></p>
     <p>©2011 JoPs / Lampin.net</p>
 </div>
