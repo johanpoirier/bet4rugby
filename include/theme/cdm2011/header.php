f4631153ca84d6bd18f4811a7a62600523747d5b
Johan Poirier <johan.poirier@gmail.com>
2015-06-22 22:32:49 +0200

    Using flexbox model to be responsive

--- include/theme/cdm2011/header.php
+++ include/theme/cdm2011/header.php
@@ -1,7 +1,6 @@
-<div id="head1">
+<header>
     <div id="logo">
         <a href="index.php"><img src="include/theme/cdm2011/images/logo.png" alt="CdM" border="0" /></a>
     </div>
     <?php include_once("pages/infos_user.php"); ?>
-</div>
-<br />
+</header>
--- pages/ranking.php
+++ pages/ranking.php
@@ -14,43 +14,38 @@ $nbJoueursActifs = $engine->getNbActivePlayers();
             </tr>
         </table>
     </div>
-</div>
 
-<div class="maincontent">		
     <table>
         <tr>
-            <td width="45" style="font-size:80%;text-align:center;"><i>Rang</i></td>
-            <td width="165" style="font-size:80%"><i>Parieur</i></span>
-            <td width="80" style="font-size:80%;text-align:center;"><i>Equipe</i></span>
-            <td width="50" style="font-size:80%;text-align:center;"><i>Points</i></td>
-            <td width="50" style="font-size:80%;text-align:center;"><i>R&eacute;sultats Exacts</i></td>
-            <td width="50" style="font-size:80%;text-align:center;"><i>Super Scores</i></td>
-            <td width="50" style="font-size:80%;text-align:center;"><i>Différence</i></td>
+            <th width="10%" style="font-size:80%;text-align:center;"><i>Rang</i></th>
+            <th width="30%" style="font-size:80%"><i>Parieur</i></th>
+            <th width="20%" style="font-size:80%;text-align:center;"><i>Equipe</i></th>
+            <th width="10%" style="font-size:80%;text-align:center;"><i>Points</i></th>
+            <th width="10%" style="font-size:80%;text-align:center;"><i>R&eacute;sultats Exacts</i></th>
+            <th width="10%" style="font-size:80%;text-align:center;"><i>Super Scores</i></th>
+            <th width="10%" style="font-size:80%;text-align:center;"><i>Différence</i></th>
         </tr>
-    </table>
 
     <!-- BEGIN users -->
-    <?php
+<?php
     foreach ($users as $user) {
-        ?>
-        <div class="list_element">
-            <table>
-                <tr>
-                    <td width="45" style="font-size:80%;text-align:center;"><strong><?php echo $user['RANK']; ?></strong> <?php echo $user['LAST_RANK']; ?></td>
-                    <td width="165" style="font-size:70%"><strong><?php echo $user['VIEW_BETS']; ?><?php echo $user['NAME']; ?><?php echo "</a>"; ?></strong> <?php echo $user['NB_BETS']; ?></td>
-                    <td width="80" style="font-size:70%;text-align:center;"><?php echo $user['TEAM']; ?></td>
-                    <td width="50" style="font-size:70%;text-align:center;"><strong><?php echo $user['POINTS']; ?></strong></td>
-                    <td width="50" style="font-size:70%;text-align:center;"><?php echo $user['NBRESULTS']; ?></td>
-                    <td width="50" style="font-size:70%;text-align:center;"><?php echo $user['NBSCORES']; ?></td>
-                    <td width="50" style="font-size:70%;text-align:center;"><?php echo $user['DIFF']; ?></td>
-                </tr>
-            </table>
-        </div>
+?>
+        <tr class="list_element">
+            <td width="10%" style="font-size:80%;text-align:center;"><strong><?php echo $user['RANK']; ?></strong> <?php echo $user['LAST_RANK']; ?></td>
+            <td width="30%" style="font-size:70%"><strong><?php echo $user['VIEW_BETS']; ?><?php echo $user['NAME']; ?><?php echo "</a>"; ?></strong> <?php echo $user['NB_BETS']; ?></td>
+            <td width="20%" style="font-size:70%;text-align:center;"><?php echo $user['TEAM']; ?></td>
+            <td width="10%" style="font-size:70%;text-align:center;"><strong><?php echo $user['POINTS']; ?></strong></td>
+            <td width="10%" style="font-size:70%;text-align:center;"><?php echo $user['NBRESULTS']; ?></td>
+            <td width="10%" style="font-size:70%;text-align:center;"><?php echo $user['NBSCORES']; ?></td>
+            <td width="10%" style="font-size:70%;text-align:center;"><?php echo $user['DIFF']; ?></td>
+        </tr>
 <?php } ?>
+
+    </table>
     <!-- END users -->
 </div>
 
-<div id="rightcolumn">
+<aside>
     <div class="tag_cloud">
         <div class="rightcolumn_headline"><h1 style="color:black;">ChatBoard</h1></div>
         <div id="tag_0" styAle="text-align:center;"><br />
@@ -64,9 +59,7 @@ $nbJoueursActifs = $engine->getNbActivePlayers();
             <!--{NAVIG}-->
         </div>
     </div>
-</div>
+</aside>
 <script type="text/javascript">
-<!--
-      loadTags();
-//-->
+    loadTags();
 </script>
