1741ac733914f8193af886d1de412973db069bc1
Johan Poirier <johan.poirier@gmail.com>
2015-06-22 13:22:36 +0200

    Fixes #6 and #9: new logo and updated link for OSS project

--- include/theme/cdm2011/footer_private.php
+++ include/theme/cdm2011/footer_private.php
@@ -1,4 +1,4 @@
 <div id="footer">
-    <p><a href="index.php">Accueil</a> | <a href="/?op=account">Mon compte</a> | <a href="/?op=rules">Règlement</a> | <a href="https://bitbucket.org/dst17/cdm2011/issues" target="_blank">Soummettre un bug sur Bitbucket</a> | <a href="mailto:johan.poirier@gmail.com">E-mail</a> | <a href="/?op=logout">Déconnexion</a></p>
-    <p>©2011 JoPs / Lampin.net</p>
+    <p><a href="index.php">Accueil</a> | <a href="/?op=account">Mon compte</a> | <a href="/?op=rules">Règlement</a> | <a href="https://github.com/johanpoirier/bet4rugby/issues/new" target="_blank">Soumettre un bug</a> | <a href="mailto:johan.poirier+cdm2015@gmail.com">E-mail</a> | <a href="/?op=logout">Déconnexion</a></p>
+    <p>©2015 JoPs</p>
 </div>
--- include/theme/cdm2011/footer_public.php
+++ include/theme/cdm2011/footer_public.php
@@ -1,4 +1,4 @@
 <div id="footer">
-    <p><a href="/?op=register">S'inscrire</a> | <a href="/?op=forgot_ids">J'ai oublié mes identifiants de connexion</a> | <a href="https://bitbucket.org/dst17/cdm2011" target="_blank">Projet sur Bitbucket</a> | <a href="mailto:johan.poirier@gmail.com">E-mail</a></p>
+    <p><a href="/?op=register">S'inscrire</a> | <a href="/?op=forgot_ids">J'ai oublié mes identifiants de connexion</a> | <a href="https://github.com/johanpoirier/bet4rugby" target="_blank">Projet Open Source</a> | <a href="mailto:johan.poirier+cdm2015@gmail.com">E-mail</a></p>
     <p>©2011 JoPs / Lampin.net</p>
 </div>
